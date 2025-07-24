402669d7ded282244e4dc27ab6668977bb6820f2
henrique vera <hverar@gmail.com>
2025-07-24 18:58:53 -0400

    alosmt finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    locale = params[:locale] || cookies[:locale] || I18n.default_locale
+    I18n.locale = locale
+    cookies[:locale] = locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
